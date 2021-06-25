145cb500179539a0d43cceefc0233a00de3df636
Saleem Younus <saleemyounus@gmail.com>
2021-06-25 18:13:19 -0500

    python-strings

---
+++ string-index.py
@@ -0,0 +1,14 @@
+#quotations/apostraphies r used interchangably depending on interfering content within string
+stringApostraphy = 'pythons for "beginners"'  
+stringQuote = "python's for beginners"
+
+stingLines = '''
+using 3 ' to create string over several lines
+secondline of string
+'''
+
+print(stringQuote[0:])
+
+another = stringQuote[:]
+
+print(another)
---
+++ string-methods.py
@@ -0,0 +1,8 @@
+course = 'python for beginners'
+print(len(course))
+print(course.upper())
+print(course.lower())
+ 
+print(course.find('n'))
+print(course.replace('p', 'r'))
+print('python' in course) #boolean expression
